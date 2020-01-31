c94f22273b4cb9ef4804e39ed78dda25ebcc22aa
Myrian Katto <56876888+myriankatto@users.noreply.github.com>
2020-01-31 16:29:31 +0000

    Add files via upload

---
+++ scripts/game.js
@@ -0,0 +1,32 @@
+const BLOCK_SIZE = 18;
+
+class Game {
+  constructor ($canvas) {
+
+  }
+
+  start () {
+
+  }
+
+  randomizeDogs() {
+
+  }
+
+  loop () {
+
+  }
+
+  runLogic () {
+
+  }
+
+  clear () {
+    const { width, height } = this.$canvas;
+    this.context.clearRect(0, 0, width, height);
+  }
+
+  draw () {
+
+  }
+}
